1721dc1ce2ee40df5cf5b78716644b364e1658bb
Jorge Castillo <jorge.castillo.prz@gmail.com>
2016-06-26 13:45:39 +0200

    refactor auth a little bit

--- app/auth/BasicAuthAction.scala
+++ app/auth/BasicAuthAction.scala
@@ -7,6 +7,10 @@ import play.mvc.{Action, Result}
 import repository.database.DBUserRepositoryComponent
 
 /**
+  * Custom Action to do basic authentication. Added to the CharacterController class through
+  * BasicAuth annotation. By doing that, all the controller exposed action methods (API calls)
+  * are authenticated.
+  *
   * @author jorge
   * @since 26/06/16
   */
@@ -15,24 +19,28 @@ class BasicAuthAction extends Action[Result] with UserController with DBUserRepo
   override def call(ctx: Context): Promise[Result] = {
     val authHeader = ctx.request().getHeader(BasicAuthAction.AUTHORIZATION)
     if (authHeader == null) {
-      ctx.response().setHeader(BasicAuthAction.WWW_AUTHENTICATE, BasicAuthAction.REALM)
-      return unauthorized
+      return unauthorized(ctx)
     }
 
     val auth = authHeader.substring(6)
     val decodedAuth = new sun.misc.BASE64Decoder().decodeBuffer(auth)
     val credString = new String(decodedAuth, "UTF-8").split(":")
-    if (credString == null || credString.length != 2) unauthorized
+    if (credString == null || credString.length != 2) {
+      return unauthorized(ctx)
+    }
 
     val username = credString(0)
     val password = credString(1)
 
     if (authenticate(username, password)) {
       delegate.call(ctx)
-    } else unauthorized
+    } else {
+      return unauthorized(ctx)
+    }
   }
 
-  def unauthorized: Promise[Result] = {
+  def unauthorized(ctx: Context): Promise[Result] = {
+    ctx.response().setHeader(BasicAuthAction.WWW_AUTHENTICATE, BasicAuthAction.REALM)
     Promise.promise(new Function0[Result]() {
       override def apply(): Result = {
         play.mvc.Results.unauthorized()
